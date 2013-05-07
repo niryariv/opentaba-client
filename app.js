4803e2e30e531f60109acddaad7b9f11a2093207
Nir Yariv <niryariv@gmail.com>
2013-05-07 16:50:55 +0300

    another typo

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ $(document).ready(function(){
 	/* deprecating for serverless
 	// wake up possibly-idling heroku dyno to make sure later requests aren't too slow
 	$.getJSON( API_URL + "wakeup" , function(){/* do nothing */})
-	*/
+	//*/
 
 	// setup a path.js router to allow distinct URLs for each block
 	Path.map("#/gush/:gush_id").to(

a0ab39ef6fb0ec871b7c862dc85f4d98a874ab48
alonisser <alonisser@gmail.com>
2013-09-14 19:37:07 +0300

    more details in testing.md and added stubs for further testing

--- tests/test_index_get_gush.js
+++ tests/test_index_get_gush.js
@@ -13,7 +13,7 @@ casper.options.logLevel = "debug";
 casper.options.verbose = true;
 casper.options.viewportSize = {width:1024, height:768};
 //Starting the tests
-casper.test.begin('Basic index.html elements test',4, function suite(test){
+casper.test.begin('Testing a specific gush plans display',4, function suite(test){
 
 	casper.on('page.init',initMock).
 	on('remote.message',log).
@@ -23,16 +23,24 @@ casper.test.begin('Basic index.html elements test',4, function suite(test){
 	
 	var hashpath = '/gush/30338';
 	var gushurl = url +'#'+ hashpath;
-
 	casper.thenOpen(gushurl).on('url.changed', initMock).wait(10000).
 	then(function(){
-		//test.assertSelectorHasText('#info','עוד מעט');
-		//this.waitForText('30338');	
-		//test.assertHttpStatus('200', 'Ajax function is returning 200');
 		test.assertExists('#info h3','the info h3 exists now');
 		test.assertSelectorHasText('#info h3','גוש 30338');
+		//casper.log('The info h3 contains the expected text');
 		test.assertElementCount('div#info tr.item',31,"31 items exists in info div as expected");
 		test.assertElementCount('div#info a',85, "85 a links should exists in info div");
+	});
+
+	var href;
+	casper.then(function(){
+		href = casper.evaluate(function(){
+			//implent a test for specifc link href
+		});
+	});
+
+	casper.then(function(){
+		//implent a test for icons
 	});
 
 	casper.run(function(){
@@ -45,13 +53,10 @@ function initMock(){
 		var server = sinon.fakeServer.create();
 		server.autoRespond = true;
 		var answer = JSON.stringify(planFixture_30338);
-		//console.log(planFixture_30338.length);
 		server.respondWith('GET', 'http://0.0.0.0:5000/gush/30338/plans',
 			[200, {"content-type":"application/json"}, answer]);
 		server.respond();
-		console.log('injected sinon');
-		//console.log(server);
-		//return server;
+		console.log('injected sinon with test fixture');
 
 	});
 	casper.log('injected sinon fakeserver now', 'debug');

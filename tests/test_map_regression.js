cf271178df3bb7a2d91bd45f5a338c348374c032
alonisser <alonisser@gmail.com>
2013-09-30 22:15:43 +0300

    added more tests to get gush

--- tests/test_map_regression.js
+++ tests/test_map_regression.js
@@ -6,32 +6,32 @@
 //Some setup //TODO: move this to a global casper config
 var url = '../index.html';
 var casper = require('casper').create({
-	viewportSize: {
-		width: 1024,
-		height: 768
-	},
-    	verbose:true,
-	logLevel:"debug",
-    	waitTimeout:12000
+    viewportSize: {
+        width: 1024,
+        height: 768
+    },
+    verbose:true,
+    logLevel:"debug",
+    waitTimeout:12000
 });
 
 //initializing phantomcss
 
 var phantomcss = require('./PhantomCSS/phantomcss.js');
 phantomcss.init({
-	libraryRoot:'./PhantomCSS',
-	screenshotRoot:'./img',
-	failedComparisonsRoot:'./fail_img'
+    libraryRoot:'./PhantomCSS',
+    screenshotRoot:'./img',
+    failedComparisonsRoot:'./fail_img'
 });
-var delay =10;
+var delay = 10;
 casper.start(url).then(function(){
-	phantomcss.screenshot('#map.leaflet-container.leaflet-fade-anim','full_map.png');
+    phantomcss.screenshot('#map.leaflet-container.leaflet-fade-anim','full_map.png');
 });
 
 casper.then(function now_check_the_screenshot(){
-	phantomcss.compareAll();
+    phantomcss.compareAll();
 }).
 run(function end_it(){
-	console.log('ending the phantomcss test');
-	phantom.exit(phantomcss.getExitStatus());
+    console.log('ending the phantomcss test');
+    phantom.exit(phantomcss.getExitStatus());
 });

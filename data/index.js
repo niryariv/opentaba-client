8113d25cbf0ec7764f0eac87378204ec1d94ca26
florpor <florist.powers@gmail.com>
2014-08-20 14:38:51 +0300

    added tveria. fabfile testing

--- data/index.js
+++ data/index.js
@@ -13,9 +13,16 @@ var municipalities = {
             35.202637
         ],
         "display": "\u05d9\u05e8\u05d5\u05e9\u05dc\u05d9\u05dd",
+        "fb_link": "https://www.facebook.com/opentaba.jerusalem",
         "file": "data/jerusalem.json",
         "server": "http://opentaba-server.herokuapp.com/",
-        "fb_link": "https://www.facebook.com/opentaba.jerusalem",
         "twitter_link": "https://twitter.com/opentaba_JLM"
+    },
+    "tveria": {
+        "center": [
+            32.786941,
+            35.529917
+        ],
+        "display": "\u05d8\u05d1\u05e8\u05d9\u05d4"
     }
 };

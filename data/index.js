676ad1e8164c82e848e2feb9566e6b81bf9e6701
florpor <florist.powers@gmail.com>
2014-08-20 15:12:00 +0300

    removed givataiim and tveria. finished fabfile testing

--- data/index.js
+++ data/index.js
@@ -1,12 +1,4 @@
 var municipalities = {
-    "givataiim": {
-        "center": [
-            32.071151,
-            34.80989
-        ],
-        "display": "\u05d2\u05d1\u05e2\u05ea\u05d9\u05d9\u05dd",
-        "file": "data/givataiim.json"
-    },
     "jerusalem": {
         "center": [
             31.783476,
@@ -17,12 +9,5 @@ var municipalities = {
         "file": "data/jerusalem.json",
         "server": "http://opentaba-server.herokuapp.com/",
         "twitter_link": "https://twitter.com/opentaba_JLM"
-    },
-    "tveria": {
-        "center": [
-            32.786941,
-            35.529917
-        ],
-        "display": "\u05d8\u05d1\u05e8\u05d9\u05d4"
     }
 };

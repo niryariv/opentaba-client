a692c48d910e0b02700470fd15a67fca18fd115d
florpor <florist.powers@gmail.com>
2014-08-25 11:23:14 +0300

    haifa index.js

---
+++ data/index.js
@@ -0,0 +1,26 @@
+var municipalities = {
+    "givataiim": {
+        "center": [
+            32.071151,
+            34.80989
+        ],
+        "display": "\u05d2\u05d1\u05e2\u05ea\u05d9\u05d9\u05dd"
+    },
+    "haifa": {
+        "center": [
+            32.796224,
+            35.002742
+        ],
+        "display": "\u05d7\u05d9\u05e4\u05d4"
+    },
+    "jerusalem": {
+        "center": [
+            31.783476,
+            35.202637
+        ],
+        "display": "\u05d9\u05e8\u05d5\u05e9\u05dc\u05d9\u05dd",
+        "fb_link": "https://www.facebook.com/opentaba.jerusalem",
+        "server": "http://opentaba-server.herokuapp.com/",
+        "twitter_link": "https://twitter.com/opentaba_JLM"
+    }
+};

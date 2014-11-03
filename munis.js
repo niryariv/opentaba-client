238a7ace13a646c2f64e3356533a1faacac90a4e
Nir Yariv <niryariv@gmail.com>
2014-11-03 18:30:53 +0200

    remove server override in jerusalem

--- munis.js
+++ munis.js
@@ -101,7 +101,6 @@ var municipalities = {
         ],
         "display": "\u05d9\u05e8\u05d5\u05e9\u05dc\u05d9\u05dd",
         "fb_link": "https://www.facebook.com/opentaba.jerusalem",
-        "server": "http://opentaba-server.herokuapp.com/",
         "twitter_link": "https://twitter.com/opentaba_JLM"
     },
     "orakiva": {
